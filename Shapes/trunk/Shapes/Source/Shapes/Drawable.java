3a9a59db8b3cddd802c3313544ef47e7b84a75e8
mak11188 <3864939+Heliosana@users.noreply.github.com>
2012-05-07 14:40:05 +0000

    first upload Shapes Project

---
+++ Shapes/trunk/Shapes/Source/Shapes/Drawable.java
@@ -0,0 +1,34 @@
+package Shapes;
+
+/**
+ * declare methods draw() and move() for its subclasses
+ * 
+ * @author (Markus Krummnacker)
+ * @version (0.3)
+ */
+public abstract class Drawable extends Object {
+	/**
+	 * abstract declaration of draw()
+	 */
+	public abstract void draw();
+
+	/**
+	 * abstract declaration of drawable()
+	 * 
+	 * @param dx
+	 *            x value to move
+	 * @param dy
+	 *            y value to move
+	 */
+	public abstract Drawable move(Point trans);
+
+	/**
+	 * rotate the point around a center point
+	 * 
+	 * @param center
+	 *            point to rotate around
+	 * @param phi
+	 *            angel to rotate
+	 */
+	public abstract Drawable rotate(Point center, double phi);
+}
