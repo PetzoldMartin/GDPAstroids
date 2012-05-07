fb557d470699c72026e8d588355e8ff1f8465f6a
Martin Petzold <aismaelinc@web.de>
2012-05-07 18:36:14 +0000

    Try Fix Rotate

---
+++ Shapes/branches/Try Fix Rotate/Shapes/Source/JUnitTest/RectangleTest.java
@@ -0,0 +1,39 @@
+package JUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RectangleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDraw() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRectanglePointDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRectanglePointDoubleDoubleColorBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRotatePointDouble() {
+		fail("Not yet implemented");
+	}
+
+}
