fedfa1a8291ea1666866fd7ea6c542399fff799f
map111ks <map111ks@fa8c7947-60ef-4644-94ff-086034abe115>
2012-05-08 21:00:51 +0000

    Spaceship and Astroid created without methods

--- Shapes/trunk/Shapes/Source/Shapes/GameController.java
+++ Shapes/trunk/Shapes/Source/Shapes/GameController.java
@@ -1,7 +1,53 @@
 package Shapes;
 
+import java.awt.Color;
 import Shapes.Sprite.*;
 
+/**
+ * class that manage the program
+ * 
+ * @author (Martin Petzold)
+ * @version (0.3)
+ */
 public class GameController {
+	public static int windowX = 380;
+	public static int windowY = 275;
+	public static Point window = new Point(windowX, windowY);
 
+	/**
+	 * @param args
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		Drawable background = new Rectangle(new Point(0, 0), window.getX(),
+				window.getY(), Color.BLACK, true);
+		background.draw();
+		SpaceShip test = new SpaceShip();
+		for (;;) {
+			for (int i = 0; i < 25; i++) {
+				test.rotate(5).draw();
+				Thread.sleep(10);
+				test.move(new Point(0, i)).draw();
+				Thread.sleep(10);
+			}
+			for (int i = 0; i < 25; i++) {
+				test.rotate(5).draw();
+				Thread.sleep(10);
+				test.move(new Point(i, 0)).draw();
+				Thread.sleep(10);
+			}
+			for (int i = 0; i < 25; i++) {
+				test.rotate(5).draw();
+				Thread.sleep(10);
+				test.move(new Point(0, -i)).draw();
+				Thread.sleep(10);
+			}
+			for (int i = 0; i < 25; i++) {
+				test.rotate(5).draw();
+				Thread.sleep(10);
+				test.move(new Point(-i, 0)).draw();
+				Thread.sleep(10);
+			}
+		}
+	}
 }
---
+++ Shapes/trunk/Shapes/Source/Shapes/SpaceShip.java
@@ -0,0 +1,18 @@
+package Shapes;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+public class SpaceShip extends Sprite {
+
+	public SpaceShip()
+	{
+	ArrayList<Point> shipList = new ArrayList<Point>();
+	shipList.add(new Point(-10, -10));
+	shipList.add(new Point(10, -10));
+	shipList.add(new Point(0, 15));
+	this.addShape(new Polygon(shipList, Color.RED, false));
+	}
+	
+
+}
