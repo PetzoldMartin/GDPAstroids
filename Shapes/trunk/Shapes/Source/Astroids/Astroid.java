b02fe7954081d5e583359e003c03aba404f4fb8b
mak11188 <3864939+Heliosana@users.noreply.github.com>
2012-05-09 08:47:57 +0000

    merged

--- Shapes/trunk/Shapes/Source/Astroids/Astroid.java
+++ Shapes/trunk/Shapes/Source/Astroids/Astroid.java
@@ -3,11 +3,16 @@ package Astroids;
 public class Astroid extends Sprite {
 	//TODO multiple Constuctors with middle point and vector input with uses super Constructor 
 
+	public Astroid() {
+		
+	}
+	
 	@Override
 	public void update() {
 		{
 			this.draw();
 			this.move(vector);
+			rotate(this.rotationPhi);
 		}
 		
 	}
