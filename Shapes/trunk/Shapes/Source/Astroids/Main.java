c94be5a24154b3864d4ba85829273d7336ddc92f
mak11188 <3864939+Heliosana@users.noreply.github.com>
2012-05-09 08:03:09 +0000

    synch with astroids from last commit

---
+++ Shapes/trunk/Shapes/Source/Astroids/Main.java
@@ -0,0 +1,73 @@
+package Astroids;
+
+import java.awt.Color;
+
+import Shapes.Drawable;
+import Shapes.Point;
+import Shapes.Rectangle;
+
+
+/**
+ * class that manage the program
+ * 
+ * @author (Markus Krummnacker)
+ * @version (0.3)
+ */
+public class Main {
+	public static int windowX = 380/2;
+	public static int windowY = 275/2;
+	public static Point window = new Point(windowX*2, windowY*2);
+
+	/**
+	 * @param args
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		Drawable background = new Rectangle(new Point(0, 0), window.getX(),
+				window.getY(), Color.BLACK, true);
+		background.draw();
+		Sprite test1 = new SpaceShip();
+		Sprite test2 = new SpaceShip();
+		test1.vector.changeSpeed(10);
+		test2.vector.changeSpeed(5);
+		long time =20;
+		for (;;) {
+			for (int i = 0; i < 25; i++) {
+				test1.rotate(5).draw();
+				Thread.sleep(time);
+				test1.update();
+				test2.update();
+				Thread.sleep(time);
+			}
+			test1.vector.changeDirection(90);
+			test2.vector.changeDirection(90);
+			for (int i = 0; i < 25; i++) {
+				test1.rotate(5).draw();
+				Thread.sleep(time);
+				test1.update();
+				test2.update();
+				Thread.sleep(time);
+			}
+			test1.vector.changeDirection(90);
+			test2.vector.changeDirection(90);
+			for (int i = 0; i < 25; i++) {
+				test1.rotate(5).draw();
+				Thread.sleep(time);
+				test1.update();
+				test2.update();
+				Thread.sleep(time);
+			}
+			test1.vector.changeDirection(90);
+			test2.vector.changeDirection(90);
+			for (int i = 0; i < 25; i++) {
+				test1.rotate(5).draw();
+				Thread.sleep(time);
+				test1.update();
+				test2.update();
+				Thread.sleep(time);
+			}
+			test1.vector.changeDirection(90);
+			test2.vector.changeDirection(90);
+		}
+	}
+}
---
+++ Shapes/trunk/Shapes/Source/Astroids/Sprite.java
@@ -0,0 +1,64 @@
+package Astroids;
+
+import java.util.ArrayList;
+import java.awt.Color;
+
+import Shapes.Circle;
+import Shapes.Drawable;
+import Shapes.Figure;
+import Shapes.Point;
+
+public abstract class Sprite extends Figure {
+	// TODO Kommentierung!
+	// TODO make it abstract!
+	public Vector vector ;
+	private Point middlePoint ;
+
+	// TODO implement an variable for rotaion that control the self rotation and
+	// rotate in update() --> usefull for Astroids! also in ship! for rotate @update()!
+	public Sprite() {
+		this.middlePoint= new Point(0, 0);
+		this.vector=new Vector(0, 0);
+		this.addShape(new Circle(1, this.getMiddlePoint(), Color.RED, false));
+		
+	}
+
+	public Sprite(Point middlePoint) {
+		this();
+		this.middlePoint = middlePoint;
+	}
+	
+	public Sprite(Point middlePoint, Vector vector){
+		this();
+		this.middlePoint= middlePoint;
+		this.vector= vector;
+	}
+
+	public abstract void update();
+//	{
+//		this.draw();
+//		this.move(vector);
+//	}
+	// TODO Wenn direction seit dem letztem Aufruf von update() veraendert wurde, dann wird shape.rotate() aufgerufen. 
+	// Dies soll also nur bei einer Veraenderung passieren damit die Shape nicht st�ndig rotiert. Zur Fortbewegung 
+	// werden das Delta-x und -y berechnet und in shape.move() verwendet. Diese Methode wird immer aufgerufen, 
+	// damit sich die Shape bei jedem update() fortbewegt (es sei denn, speed ist 0).
+	
+	public Sprite move() {
+		return (Sprite) super.move(vector);
+	}
+
+	public Drawable rotate(double phi) {
+		return this.rotate(middlePoint, phi);
+	}
+
+
+	public Point getMiddlePoint() {
+		return middlePoint;
+	}
+
+	public void setMiddlePoint(Point middlePoint) {
+		this.middlePoint = middlePoint;
+	}
+
+}
