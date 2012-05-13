5b618b8541bd2a1d57aec6c3f27b5558e82393b5
map111ks <map111ks@fa8c7947-60ef-4644-94ff-086034abe115>
2012-05-13 15:53:54 +0000

    try to copy tag 0.1 to trunk

---
+++ Shapes/trunk/Shapes/Source/JUnitTest/MainTest.java
@@ -0,0 +1,32 @@
+package JUnitTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class MainTest extends TestSuite {
+	// TODO EclEmma Installieren und Konfigurieren
+	// The Eclipse preferences section General → Appearance → Editors → Text
+	// Editors → Annotations allows to modify the visual representation of
+	// coverage highlighting. The corresponding entries are:
+	//
+	// Full Coverage → Farbe anpassen
+	// Partial Coverage→Farbe anpassen
+	// No Coverage→Farbe anpassen
+	//
+	// Coverage Window einfügen
+	// Window=>Show view=> others------ Java=>Coverage
+	// wenn bunt stört dort remove all sessions auswählen
+
+	public static Test suite() {
+		TestSuite mySuite = new TestSuite("Shapes Test-Suite");
+		mySuite.addTestSuite(CircleTest.class);
+		mySuite.addTestSuite(LineTest.class);
+		mySuite.addTestSuite(PointTest.class);
+		mySuite.addTestSuite(PolygonTest.class);
+		mySuite.addTestSuite(RectangleTest.class);
+		mySuite.addTestSuite(SpriteTest.class);
+
+		// ... weitere Testklassen hinzufügen
+		return mySuite;
+	}
+}
