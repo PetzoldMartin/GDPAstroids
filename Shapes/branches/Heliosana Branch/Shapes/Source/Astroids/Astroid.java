c93a77f255d4c5014d74f3707da7639ee7d512b5
mak11188 <mak11188@fa8c7947-60ef-4644-94ff-086034abe115>
2012-05-16 13:42:45 +0000

    try to implemet destroy() in Astroids

--- Shapes/branches/Heliosana Branch/Shapes/Source/Astroids/Astroid.java
+++ Shapes/branches/Heliosana Branch/Shapes/Source/Astroids/Astroid.java
@@ -27,6 +27,7 @@ public class Astroid extends Sprite {
 		this.addShape(new Polygon(astroList, Color.WHITE, false));
 		this.setVector(new Vector(1, Math.random() * 360));
 		this.rotationPhi = Math.random() * 12-6;
+		//TODO places Astroids only at the corners!
 		this.move(new Point(Math.random() * gameController.getWindowX() * 2
 				- gameController.getWindowX(), Math.random()
 				* gameController.getWindowY() * 2 - gameController.getWindowY()));
@@ -35,5 +36,11 @@ public class Astroid extends Sprite {
 	public void update() {
 		super.update();
 		this.rotate(this.rotationPhi);
-	}	
+	}
+	public void destroy() {
+		for (int i=0;i<2;i++) {
+		new Astroid(edge/2,radius*2/3);
+		}
+		this.remove();
+	}
 }
