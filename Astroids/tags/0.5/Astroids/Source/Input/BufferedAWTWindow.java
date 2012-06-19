e4bc0c7f349d2fb18d72e1cc52e667ccb3c789db
mak11188 <3864939+Heliosana@users.noreply.github.com>
2012-06-19 09:41:32 +0000

    pre alpha

---
+++ Astroids/tags/0.5/Astroids/Source/Input/BufferedAWTWindow.java
@@ -0,0 +1,62 @@
+package Input;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Panel;
+
+import Astroids.GameController;
+
+public class BufferedAWTWindow extends Panel {
+
+	/**
+	 * Ein Gepuffertes AWT-Panel für eine Einfache Grafische Ausgabe
+	 * mit Voreinstellungen für die Textausgabe
+	 */
+	private static final long serialVersionUID = 1L;
+	protected GameController gameController;
+	Image bufImage;//
+	Graphics bufG;//
+	private Font l;
+	private int fontSize=20;
+	protected String name;
+	
+
+	public BufferedAWTWindow(String name, GameController gameController) {
+		this.gameController = gameController;
+		this.setBackground(Color.BLACK);
+		l = new Font("Arial", Font.BOLD, fontSize);
+		this.setPreferredSize(new Dimension(300, fontSize+20));
+		this.setFont(l);
+		this.name=name;
+
+	}
+	
+	@Override
+	/**
+	 * der Puffer für die Grafische AWT Ausgabe
+	 */
+	public void update(Graphics g) {
+
+		int w = this.getSize().width;
+		int h = this.getSize().height;
+
+		if (bufImage == null) {
+			bufImage = this.createImage(w, h);
+			bufG = bufImage.getGraphics();
+		}
+
+		bufG.setColor(this.getBackground());
+		bufG.fillRect(0, 0, w, h);
+
+		bufG.setColor(this.getForeground());
+
+		paint(bufG);
+
+		g.drawImage(bufImage, 0, 0, this);
+
+	}
+
+}
---
+++ Astroids/tags/0.5/Astroids/Source/Shapes/Point.java
@@ -0,0 +1,120 @@
+package Shapes;
+
+/**
+ * class that manage x and y cordinates of points as an object
+ * 
+ * @author Markus Krummnacker
+ * @version (0.3)
+ */
+public class Point {
+	protected double x, y;
+
+	/**
+	 * Constructor for objects of class Point
+	 * 
+	 * @param x
+	 *            x-cordinate of the point
+	 * @param y
+	 *            y-cordinate of the point
+	 */
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * move the point by dx & dy on Whiteboard
+	 * 
+	 * @param dX
+	 *            delta x to translate
+	 * @param dY
+	 *            y to translate
+	 */
+	public Point move(double dX, double dY) {
+		this.x += dX;
+		this.y += dY;
+		return this;
+	}
+
+	/**
+	 * move the point by Point(delta x , delta y) on Whiteboard
+	 * 
+	 * @param trans
+	 *            point to translate with dx and dy
+	 */
+	public Point move(Point trans) {
+		return this.move(trans.getX(), trans.getY());
+	}
+
+	/**
+	 * rotate the point around a center point
+	 * 
+	 * @param center
+	 *            point to rotate around
+	 * @param phi
+	 *            angel to rotate
+	 */
+	public void rotate(Point center, double phi) {
+		if (!center.equals(this)) {
+			phi = Math.toRadians(phi);
+			Point temp = this.copy().move(
+					new Point(-1 * center.getX(), -1 * center.getY()));
+			this.x = temp.getX() * Math.cos(phi) + temp.getY() * Math.sin(phi);
+			this.y = -1 * temp.getX() * Math.sin(phi) + temp.getY()
+					* Math.cos(phi);
+			this.move(center);
+		}
+	}
+
+	/**
+	 * @return an inverted Point of this Point
+	 */
+	public Point invert() {
+		return new Point(-getX(), -getY());
+	}
+
+	/**
+	 * @return a new point that is a copy of this one
+	 */
+
+	public Point copy() {
+		return new Point(getX(), getY());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj instanceof Point) {
+			Point otherPoint = (Point) obj;
+			if (this.getX() == (otherPoint.getX())
+					& this.getY() == (otherPoint.getY())) {
+				return true;
+			} else
+				return false;
+		} else
+			return false;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+}
